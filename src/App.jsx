a8480b296632045fe76676b26dfd48418b338367
Uğur Akçay <akcayugur01@gmail.com>
2024-02-24 14:12:42 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import classNames from "classnames";
+import classnames from "classnames";
 import { useState, useEffect } from "react";
 
 const customArray = Array.from({ length: 5 }, () =>
@@ -28,9 +28,10 @@ function App() {
       );
       setArray(newBoard);
       setCurrentRow(currentRow + 1);
-
-      const isWon = newBoard.every((row) => row.every((tile) => tile === "✓"));
-      setIsGameWon(isWon);
+      const isWon = newBoard.every((row) => row.every((tile) => tile !== true));
+      if (isWon) {
+        setIsGameWon(true);
+      }
     }
   };
 
@@ -66,19 +67,22 @@ function App() {
     setArray(newArray);
     setCurrentRow(0);
     setIsGameEnd(false);
+    setIsGameWon(false);
   };
 
   return (
-    <div className="w-full h-[100vh] flex flex-col items-center justify-center ">
-      <div className="w-[260px] flex flex-col gap-y-2 ">
-        <div className="grid grid-cols-3 p-6 bg-zinc-300 gap-4  rounded-lg">
+    <div className="w-full h-[100vh] bg-slate-800 flex flex-col items-center justify-center ">
+      <div className="w-[260px] flex flex-col ">
+        <div className="grid grid-cols-3 p-6 bg-gray-400 gap-4  rounded-md">
           {array.map((row, rowIndex) =>
             row.map((tile, colIndex) => (
               <div
-                className={classNames(
-                  "flex items-center justify-center w-16 h-16 bg-gray-500 text-white  hover:scale-95 shadow-lg border-4 ",
+                className={classnames(
+                  "flex items-center justify-center w-16 h-16 bg-slate-500 text-white rounded-md  hover:scale-95 shadow-lg border-4 ",
                   {
-                    "border-green-500 ": tile === "✓",
+                    "border-green-500":
+                      tile === "✓" || (isGameEnd == true && tile === true),
+                    "bg-orange-400": rowIndex == currentRow && !isGameEnd,
                     "border-red-500 ": tile === "X",
                     "pointer-events-none": isGameEnd == true,
                   }
@@ -94,19 +98,25 @@ function App() {
             ))
           )}
         </div>
-        {isGameEnd && (
-          <div className="w-full flex items-center justify-between">
-            <div>You Lose :(</div>
+        <div
+          className={classnames(
+            "w-full h-12 flex items-center justify-between invisible text-white text-lg font-mono",
+            {
+              "!visible": isGameEnd || isGameWon,
+            }
+          )}
+        >
+          {isGameEnd && <div>You Lose :(</div>}
+          {isGameWon && <div>You Win :)</div>}
+          {(isGameEnd || isGameWon) && (
             <button
               className="border border-green-500 px-2 py-1 rounded-md bg-green-500 text-white shadow-xl"
               onClick={restartGame}
             >
               Restart
             </button>
-          </div>
-        )}
-
-        {isGameWon && <div>helal</div>}
+          )}
+        </div>
       </div>
     </div>
   );
